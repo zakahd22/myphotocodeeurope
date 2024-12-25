924b2b86bf639ee101ac0663c70f480851e6ca68
victorcarreteroGit <victor.carretero@treemes.com>
2024-12-25 11:42:09 +0100

    logsMyPC

--- common/global.php
+++ common/global.php
@@ -5,7 +5,7 @@ ini_set('ignore_repeated_errors', TRUE); // always use TRUE
 ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
 ini_set('log_errors', TRUE); // Error/Exception file logging engine.
 ini_set('error_log', "../logsMyPC/log_reporting_common_global-".date("Ymd").".dat"); // Logging file path
-error_log( "checking path" . MYVERSION );
+error_log( "checking path" );
 
 ob_start();
 function is_session_started(){
