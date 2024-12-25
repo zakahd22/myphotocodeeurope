8c33232fe5ce04bd55c105e62b99d1d4bfb6d49b
victorcarreteroGit <victor.carretero@treemes.com>
2024-12-25 11:21:41 +0100

    error_log

--- app/photobooth/common.php
+++ app/photobooth/common.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
 ini_set('ignore_repeated_errors', TRUE); // always use TRUE
 ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
 ini_set('log_errors', TRUE); // Error/Exception file logging engine.
-ini_set('error_log', "logsMyPC/log_reporting-".date("Ymd").".dat"); // Logging file path
+ini_set('error_log', "logsMyPC/log_reporting_app_photobooth-".date("Ymd").".dat"); // Logging file path
 
 
 //error_reporting(0);
--- common/global.php
+++ common/global.php
@@ -1,4 +1,12 @@
 <?php
+error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
+//error_reporting(E_ALL ^ E_WARNING); // Error/Exception engine, E_ALL except Warnings
+ini_set('ignore_repeated_errors', TRUE); // always use TRUE
+ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
+ini_set('log_errors', TRUE); // Error/Exception file logging engine.
+ini_set('error_log', "logsMyPC/log_reporting_common_global-".date("Ymd").".dat"); // Logging file path
+
+
 ob_start();
 function is_session_started(){
     if ( php_sapi_name() !== 'cli' ) {
