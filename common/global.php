27898b8a558e86da7efb3bd304673fa3cbb9f243
victorcarreteroGit <victor.carretero@treemes.com>
2024-12-27 18:21:58 +0100

    des de PB - event_init

--- common/global.php
+++ common/global.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
 ini_set('ignore_repeated_errors', TRUE); // always use TRUE
 ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
 ini_set('log_errors', TRUE); // Error/Exception file logging engine.
-ini_set('error_log', "../logsMyPC/log_reporting_common_global-".date("Ymd").".dat"); // Logging file path
+ini_set('error_log', __DIR__ . "../logsMyPC/log_reporting_common_global-".date("Ymd").".dat"); // Logging file path
 error_log( "TO_DELETE checking path" );
 
 //202412logsMyPC ob_start();
