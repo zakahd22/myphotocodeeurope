edaea905f124581c740f7e56dd5cc40e56923e96
victorcarreteroGit <victor.carretero@treemes.com>
2025-02-14 14:48:48 +0100

    //20250214whatsapp

--- sections/totalshare/templates/whatsapp/enviar.php
+++ sections/totalshare/templates/whatsapp/enviar.php
@@ -1,7 +1,11 @@
 ﻿<?php
 $code = $code;
 
-$message = "Check this out! I took this photo at a DC Photobooth.\n\nClick on the link to see your photo https://www.myphotocode.com/index.php?code=$code&method=sms&v=4\n\nCome visit our DC Photobooth.";
+//20250214whatsapp $message = "Check this out! I took this photo at a DC Photobooth.\n\nClick on the link to see your photo https://www.myphotocode.com/index.php?code=$code&method=sms&v=4\n\nCome visit our DC Photobooth.";
+
+
+$message = "We are sending you this message because you have requested to share a photo of a photo booth.\n\nYou can see it at:\n https://www.myphotocode.com/photo/$code";//20250214
+
 
 $statesuccess="6";
 $statefailure="5";
