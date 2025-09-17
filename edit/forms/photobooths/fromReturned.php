f82b55d988eea9440702255dbe044056c50492dd
Abdo <abdo@dc-image.com>
2025-09-17 12:21:12 +0200

    fix: Repair and enhance photobooth return workflow
    - Fix missing "To Distributor Stock" button in return workflow UI
    - Correct distributor assignment for returned booths (status 4 -> 2)
    - Update component tracking when reassigning to new distributor
    - Fix SQL error in data_distribuidor field update
    - Add proper history tracking for booth status transitions
    - Enable complete workflow: return -> distributor -> new customer
      without manual status changes
    
    This change allows sales/production teams to properly process
    returned booths through the system, maintaining accurate distributor
    records and inventory tracking.

--- edit/forms/photobooths/fromReturned.php
+++ edit/forms/photobooths/fromReturned.php
@@ -10,7 +10,7 @@ $content .= "<div class='popup-text popup-margin-bottom'>";
 $content .= "</div>";
 
 $content .= "<div style='width:450px;' class='popup-margin-top popup-center'>";
-//$content .= "<img src='images/web/toDistributorStock.jpg' style='width:28%;float:right;display:inline;border:10px outset gray;cursor:pointer;' onclick='edit(58 , $ID);'>";
+$content .= "<img src='images/web/toDistributorStock.jpg' style='width:150px; height:150px;display:inline;border:10px outset gray;cursor:pointer;margin-right:10px;' title='To Distributor Stock' onclick='edit(58 , $ID);'>";
 $content .= "<img src='images/web/toIncomplete.jpg' style='width:150px; height:150px;display:inline;border:10px outset gray;cursor:pointer;margin-right:10px;' title ='To Incomplet Product' onclick='edit(51 , $ID);'>";
 $content .= "<img src='images/web/toDamage.jpg' style='width:150px; height:150px;display:inline;border:10px outset gray;cursor:pointer;margin-right:10px;' onclick='edit(48 , $ID);'>";
 $content .= "</div>";
